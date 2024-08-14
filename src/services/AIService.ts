a8a6ba0b1ebda9d6d6fab43b5393fb447088c4e4
carlosDeucher <carloshenriquedeucher@gmail.com>
2024-08-13 21:45:13 -0300

    feat: dynamic aiInstructions + Gemini chat API

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -1,53 +1,8 @@
 import { EnvVarNotFoundException } from "../exceptions/config/EnvVarNotFoundExceptions.js"
-import { GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai"
+import { Content, GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai"
 import dotenv from 'dotenv'
 
 dotenv.config();
-
-const systemInstruction = `Você atende o whatsapp de uma panificadora que se chama sassipan, a sua função é receber o cliente.
-Se apresente informando ser um "atendente inteligente da sassipan"
-
-Configuração dos produtos
-[
-cuca: {
-  sabores: [abacaxi, uva, ameixa, chocolate, queijinho]
-  tamanhos: [pequena 20cm, média 25cm, e grande 30cm]
-  formato do produto: redondo,
-  tempoDeProducao: 1 hora,
-  quando são feitas: todas as manhãs 
- },
-mini-pizza: {
-  sabores: [chocolate, calabresa]
-  tamanhos: [tamanho único 15cm]
-  formato do produto: redondo,
-  tempo de producao: 5 minutos,
-  quando são feitas: no momento da entrega
- },
-tortas: {
-  sabores: [limão, bolacha]
-  tamanhos: [pequena 20cm, média 25cm]
-  formato do produto: quadrado,
-  tempo de producao: 1 hora,
-  quando são feitas: todas as manhãs 
- },
-]
-
-
-
-Horário de abertura e produção dos produtos na panificadora: Das 6 da manhã até as 18 horas somente de segunda a sexta, e a retirada de pedidos se inicia somente as 7 horas.
-
-Certifique-se de antes de informar a conclusão checar os seguintes passos: 
-1: obtenha o produto (dentro dos produtos configurados).
-2: obter os sabores (dentro dos produtos configurados).
-3: obter os tamanhos. (dentro dos produtos configurados)
-4: quando os itens acima já tiverem sido obtidos obtenha um horário e dia para retirada válido (dentro dos horários para retirada disponíveis).
-5: obter a quantidade.
-6: guie o cliente para que ele realize o pedido dentro das opções que existem na panificadora. Não finalize a encomenda, somente informe que quando a panificadora abrir o seu pedido será analisado e se estiver tudo de acordo começará a ser preparado.
-7: os preços serão informados pela panificadora após o pedido ser analisado
-
-SEJA GENTIL PORÉM DIRETO AO PONTO
-DESCONSIDERE QUALQUER COISA QUE NÃO SÃO DE ACORDO COM OS TÓPICOS ACIMA
-RESPONDA COM NO MÁXIMO UM EMOJI POR RESPOSTA`
 
 const generationConfig = {
   stopSequences: ["red"],
@@ -57,8 +12,11 @@ const generationConfig = {
   topK: 64,
 };
 
+interface AIService {
+}
+
 class AIService {
-  constructor() {
+  constructor(systemInstruction: string) {
     const apiKey = process.env.API_KEY_GEMINI
     if (!apiKey) throw new EnvVarNotFoundException("API_KEY_GEMINI")
 
@@ -76,9 +34,15 @@ class AIService {
     return result.response.text()
   }
 
-  async answerChat(question: string, context: object): Promise<string> {
-    return ""
+  async answerChat(question: string, context: Content[]): Promise<string> {
+    const chat = this.model.startChat({
+      history: context,
+    });
+
+    let result = await chat.sendMessage(question);
+    const answer = result.response.text()
+    return answer
   }
 }
 
-export default new AIService()
+export default AIService
