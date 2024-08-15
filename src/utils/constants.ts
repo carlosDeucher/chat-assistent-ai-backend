212412f7764979d3582df08ff8d1c97d9774f79f
carlosDeucher <carloshenriquedeucher@gmail.com>
2024-08-14 21:44:26 -0300

    feat: date constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+// 12 months * 30 days * 24 hours * 60 minutes * 60 seconds
+export const ONE_YEAR_IN_SECONDS = 12 * 30 * 24 * 60 * 60
+
+// 8 minutes * 60 seconds
+export const EIGHT_MIN_IN_SECONDS = 8 * 60
