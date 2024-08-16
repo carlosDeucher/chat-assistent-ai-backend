2e8c1aa8ab031c0f23c7937633f2d43fbe793a68
carlosDeucher <carloshenriquedeucher@gmail.com>
2024-08-16 09:51:49 -0300

    refactor: remove classe filha

--- src/exceptions/config/EnvVarNotFoundExceptions.ts
+++ src/exceptions/config/EnvVarNotFoundExceptions.ts
@@ -1,11 +1,7 @@
 import { createError } from '@fastify/error'
 
-export class EnvVarNotFoundException extends createError(
+export const EnvVarNotFoundException = createError(
   'ERR_ENV_VAR_NOT_FOUND',
   'Variável de Ambiente não encontrada:',
   500,
-) {
-  constructor(name: string) {
-    super(name)
-  }
-}
+)
