573ff513d9d02280bd02d1ef1b287fd725e00f71
carlosDeucher <carloshenriquedeucher@gmail.com>
2024-08-30 11:07:14 -0300

    feat: gemini-1.5-flash

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -1,5 +1,11 @@
-import { EnvVarNotFoundException } from "../exceptions/config/EnvVarNotFoundExceptions.js"
-import { Content, GenerativeModel, GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai"
+import { EnvVarNotFoundException } from "../exceptions/config/EnvVarNotFoundExceptions.js";
+import {
+  Content,
+  GenerativeModel,
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
 
 const generationConfig = {
   stopSequences: ["red"],
@@ -9,67 +15,83 @@ const generationConfig = {
   topK: 64,
 };
 
-const safetySettings = [{
-  category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-  threshold: HarmBlockThreshold.BLOCK_NONE
-},
-{
-  category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-  threshold: HarmBlockThreshold.BLOCK_NONE
-},
-{
-  category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-  threshold: HarmBlockThreshold.BLOCK_NONE
-},
-{
-  category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-  threshold: HarmBlockThreshold.BLOCK_NONE
-},
-{
-  category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-  threshold: HarmBlockThreshold.BLOCK_NONE
-}]
+const safetySettings = [
+  {
+    category:
+      HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category:
+      HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category:
+      HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category:
+      HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category:
+      HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
 
 type MessageParam = {
-  id: string
-  content: string
-}
+  id: string;
+  content: string;
+};
 
 class AIService {
   constructor(systemInstruction: string) {
-    const apiKey = process.env.API_KEY_GEMINI
-    if (!apiKey) throw new EnvVarNotFoundException("API_KEY_GEMINI")
+    const apiKey = process.env.API_KEY_GEMINI;
+    if (!apiKey)
+      throw new EnvVarNotFoundException(
+        "API_KEY_GEMINI"
+      );
 
     const genAI = new GoogleGenerativeAI(apiKey);
 
     this.model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro",
-      systemInstruction, generationConfig,
+      model: "gemini-1.5-flash",
+      systemInstruction,
+      generationConfig,
       //  safetySettings
     });
-
   }
 
-  private model: GenerativeModel
-
+  private model: GenerativeModel;
 
   async answer(prompt: string): Promise<string> {
-    const result = await this.model.generateContent(prompt)
-    return result.response.text()
+    const result =
+      await this.model.generateContent(prompt);
+    return result.response.text();
   }
 
-  async answerChat(messages: MessageParam[], context: Content[]): Promise<string> {
-
+  async answerChat(
+    messages: MessageParam[],
+    context: Content[]
+  ): Promise<string> {
     const chat = this.model.startChat({
       history: context,
     });
 
-    const question = messages.map((message) => message.content + ".\n").join()
+    console.log("messages", messages);
+
+    const question = messages
+      .map((message) => message.content + ".\n")
+      .join();
 
     let result = await chat.sendMessage(question);
-    const answer = result.response.text()
-    return answer
+    const answer = result.response.text();
+    return answer;
   }
 }
 
-export default AIService
+export default AIService;
